9e96e9297be1975743355d769906a1aa626b4e15
Austin L <55158761+omple@users.noreply.github.com>
2022-09-15 22:38:27 -0400

    Can ID Changed, Switched to MK4
    reordered can id and changed to mk4 from mk3 module creators

--- SwerveSpecialties2022/src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ SwerveSpecialties2022/src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -7,6 +7,7 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.sensors.PigeonIMU;
 import com.kauailabs.navx.frc.AHRS;
 import com.swervedrivespecialties.swervelib.Mk3SwerveModuleHelper;
+import com.swervedrivespecialties.swervelib.Mk4SwerveModuleHelper;
 import com.swervedrivespecialties.swervelib.SdsModuleConfigurations;
 import com.swervedrivespecialties.swervelib.SwerveModule;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -103,13 +104,13 @@ public class DrivetrainSubsystem extends SubsystemBase {
     // you MUST change it. If you do not, your code will crash on startup.
     // FIXME Setup motor configuration
     //m_frontLeftModule = Mk3SwerveModuleHelper.createFalcon500(
-        m_frontLeftModule = Mk3SwerveModuleHelper.createNeo(
+        m_frontLeftModule = Mk4SwerveModuleHelper.createNeo(
             // This parameter is optional, but will allow you to see the current state of the module on the dashboard.
             tab.getLayout("Front Left Module", BuiltInLayouts.kList)
                     .withSize(2, 4)
                     .withPosition(0, 0),
             // This can either be STANDARD or FAST depending on your gear configuration
-            Mk3SwerveModuleHelper.GearRatio.STANDARD,
+            Mk4SwerveModuleHelper.GearRatio.L1,
             // This is the ID of the drive motor
             FRONT_LEFT_MODULE_DRIVE_MOTOR,
             // This is the ID of the steer motor
@@ -122,11 +123,12 @@ public class DrivetrainSubsystem extends SubsystemBase {
 
     // We will do the same for the other modules
     //m_frontRightModule = Mk3SwerveModuleHelper.createFalcon500(
-        m_frontRightModule = Mk3SwerveModuleHelper.createNeo(
+        
+        m_frontRightModule = Mk4SwerveModuleHelper.createNeo(
             tab.getLayout("Front Right Module", BuiltInLayouts.kList)
                     .withSize(2, 4)
                     .withPosition(2, 0),
-            Mk3SwerveModuleHelper.GearRatio.STANDARD,
+            Mk4SwerveModuleHelper.GearRatio.L1,
             FRONT_RIGHT_MODULE_DRIVE_MOTOR,
             FRONT_RIGHT_MODULE_STEER_MOTOR,
             FRONT_RIGHT_MODULE_STEER_ENCODER,
@@ -134,11 +136,11 @@ public class DrivetrainSubsystem extends SubsystemBase {
     );
 
     //m_backLeftModule = Mk3SwerveModuleHelper.createFalcon500(
-        m_backLeftModule = Mk3SwerveModuleHelper.createNeo(
+        m_backLeftModule = Mk4SwerveModuleHelper.createNeo(
             tab.getLayout("Back Left Module", BuiltInLayouts.kList)
                     .withSize(2, 4)
                     .withPosition(4, 0),
-            Mk3SwerveModuleHelper.GearRatio.STANDARD,
+            Mk4SwerveModuleHelper.GearRatio.L1,
             BACK_LEFT_MODULE_DRIVE_MOTOR,
             BACK_LEFT_MODULE_STEER_MOTOR,
             BACK_LEFT_MODULE_STEER_ENCODER,
@@ -146,11 +148,11 @@ public class DrivetrainSubsystem extends SubsystemBase {
     );
 
     //m_backRightModule = Mk3SwerveModuleHelper.createFalcon500(
-        m_backRightModule = Mk3SwerveModuleHelper.createNeo(
+        m_backRightModule = Mk4SwerveModuleHelper.createNeo(
             tab.getLayout("Back Right Module", BuiltInLayouts.kList)
                     .withSize(2, 4)
                     .withPosition(6, 0),
-            Mk3SwerveModuleHelper.GearRatio.STANDARD,
+            Mk4SwerveModuleHelper.GearRatio.L1,
             BACK_RIGHT_MODULE_DRIVE_MOTOR,
             BACK_RIGHT_MODULE_STEER_MOTOR,
             BACK_RIGHT_MODULE_STEER_ENCODER,
